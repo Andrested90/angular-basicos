3e2ef87ac266116f6fa3e10d84a92dbf16d761f4
Andres Estrada <andrested90@gmail.com>
2021-09-09 20:41:42 -0500

    primer commit

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,36 @@
+
+
+import {Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector:'app-heroe',
+    templateUrl:'heroe.component.html'    
+    
+})
+
+export class HeroeComponent{
+nombre:string ='Iroman'
+edad:number= 45;
+
+obtenerNombre() :string
+{
+  return ` ${this.nombre} - ${this.edad} `
+}
+
+get nombreCapitalizado()
+{
+    return this.nombre.toUpperCase();
+}
+
+cambiarNombre():void
+{
+ this.nombre='Spiderman'
+}
+
+cambiarEdad():void
+{
+ this.edad=30
+}
+
+
+}
